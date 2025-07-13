3cc771536e2dd3aa0619aebc3c7aec2401f186f7
bubblegumbuilder <harvey.basinga.workstation@gmail.com>
2025-07-14 07:24:08 +0800

    comments added to script

--- backend/backendScript.py
+++ backend/backendScript.py
@@ -1,6 +1,8 @@
 # I screwed up with the last backend file so I'm remaking it all over again
-
+#The line below allows the script to skim through the operating system directory. of course, we can change that to just detect the web files and the sql, but for prototype reasons, yeah
 import os
+#csv for prototype for now
 import csv
+#This is what we will use for the whole code. flask, exclusively. 
 from flask import Flask, request, redirect, send_from_directory
 
